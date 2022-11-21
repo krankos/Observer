a22ada7cf540997f5527f444d2cb569b8dbc1254
Khalil Bessaad <bessaadkhalil@gmail.com>
2022-11-21 10:30:11 +0100

    more than one observer

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,13 @@
 public class Test {
     public static void main(String[] args) {
         Timer te = new Timer();
-        Toto to = new Toto("Observer 1");
+        Toto to1 = new Toto("Observer 1");
+        Toto to2 = new Toto("Observer 2");
 
         System.out.println("La valeur initiale est " + te.getX());
-        te.addObserver(to);
-        te.setX(5);
+        te.addObserver(to1);
+        te.addObserver(to2);
+        te.setX(10);
     }
 
 }
--- src/Timer.java
+++ src/Timer.java
@@ -1,6 +1,9 @@
 import java.util.Observable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Timer extends Observable {
+    List<Toto> myObservers = new ArrayList<Toto>();
     private int n = 0;
 
     public Timer() {
@@ -9,11 +12,27 @@ public class Timer extends Observable {
 
     public void setX(int x) {
         n = x;
-        setChanged();
-        notifyObservers();
+        // setChanged();
+        // notifyObservers();
+        notifier();
+
+    }
+
+    public void notifier() {
+        for (Toto to : myObservers) {
+            to.update(this, to);
+        }
     }
 
     public int getX() {
         return n;
+    }
+
+    public void addObserver(Toto to) {
+        myObservers.add(to);
+    }
+
+    public void deleteObserver(Toto to) {
+        myObservers.remove(to);
     }
 }
